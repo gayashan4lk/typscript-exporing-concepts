b7f17e5cde469db3508da141aebd4e688d543705
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-01-05 00:47:59 +0530

    type annotations and default parameter values

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,11 +1,33 @@
 function startGame() {
-	let playerName: string = 'Johnny';
+	let playerName: string | undefined = getInputValue('playername');
 	logPlayer(playerName);
-	logPlayer(undefined);
+	postScore(25, playerName);
 }
 
 function logPlayer(name?: string) {
 	console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+	const inputElement: HTMLInputElement = document.getElementById(
+		elementId
+	) as HTMLInputElement;
+
+	if (inputElement.value == '') {
+		return undefined;
+	}
+	return inputElement.value;
+}
+
+function postScore(
+	score: number,
+	playerName: string = 'Anonymous player'
+): void {
+	const scoreElement: HTMLElement | null = document.getElementById(
+		'postedScores'
+	) as HTMLElement;
+
+	scoreElement!.innerText = `${playerName} : ${score}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
