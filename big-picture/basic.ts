bf8947ed4c1b3f33f1de0169fcbe302132b13a83
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-01-04 09:56:38 +0530

    inheritance with ts

--- big-picture/basic.ts
+++ big-picture/basic.ts
@@ -10,8 +10,24 @@ class Customer {
 	}
 }
 
+class Student extends Customer {
+	school: string;
+
+	constructor(name: string, school: string) {
+		super(name);
+		this.school = school;
+	}
+
+	greet() {
+		return `Hello, My name is ${this.name}. I'm a student at ${this.school}.`;
+	}
+}
+
 let firstCustomer = new Customer('Alice');
 let newMessage: string = firstCustomer.greet();
 
+let firstStudent = new Student('Micheal', 'Harvard');
+let studentMessage: string = firstStudent.greet();
+
 let webHeading = document.querySelector('h1');
-webHeading!.textContent = newMessage;
+webHeading!.textContent = studentMessage;
--- js/basic.js
+++ js/basic.js
@@ -7,7 +7,18 @@ class Customer {
         return `Hello, My name is ${this.name}`;
     }
 }
+class Student extends Customer {
+    constructor(name, school) {
+        super(name);
+        this.school = school;
+    }
+    greet() {
+        return `Hello, My name is ${this.name}. I'm a student at ${this.school}.`;
+    }
+}
 let firstCustomer = new Customer('Alice');
 let newMessage = firstCustomer.greet();
+let firstStudent = new Student('Micheal', 'Harvard');
+let studentMessage = firstStudent.greet();
 let webHeading = document.querySelector('h1');
-webHeading.textContent = newMessage;
+webHeading.textContent = studentMessage;
